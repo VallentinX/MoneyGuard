f8c04ff4de6293839be7e2871f647c80b1359fac
VallentinX <valentin.cristian_98@icloud.com>
2024-03-28 12:30:23 +0200

    2nd Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
+import Modal from './Modal/Modal.jsx';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Modal />
+    </>
   );
 };
