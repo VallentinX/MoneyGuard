6a3a2f9e24e10d1152b5b09789972a1ede0df2ec
VallentinX <valentin.cristian_98@icloud.com>
2024-04-02 19:56:13 +0300

    456

--- src/pages/HomeTab/HomeTab.styled.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import { StyledContainer } from 'styles/GlobalStyles';
-
-export const StyledHomeTabContainer = styled(StyledContainer)`
-  @media only screen and (min-width: 768px) {
-    padding: 0;
-  }
-`;
