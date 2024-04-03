756f64c315566e29f6aa434d486ff4eef6a223de
VallentinX <valentin.cristian_98@icloud.com>
2024-04-03 17:58:43 +0300

    rth

--- src/index.js
+++ src/index.js
@@ -7,27 +7,4 @@ import { App } from 'components/App';
 // import { theme } from './styles/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider>
-    <ThemeProvider theme={theme}>
-      <BrowserRouter basename="project-money-guard">
-        <PersistGate loading={null} persistor={persistor}>
-          <App />
-          <ToastContainer
-            position="top-right"
-            autoClose={3000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="light"
-          />
-          <GlobalStyles />
-        </PersistGate>
-      </BrowserRouter>
-    </ThemeProvider>
-  </Provider>
-);
+root.render(<App />);
